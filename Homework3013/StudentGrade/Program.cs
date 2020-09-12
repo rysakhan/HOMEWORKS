9c7718e0b437ffc657e6173663ba78846293f76a
Rysa Khan <rysakhansc@ou.edu>
2020-09-11 23:53:16 -0500

    finished student grades

---
+++ Homework3013/StudentGrade/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StudentGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            string studentId = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            double hw = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            double participation = Convert.ToDouble(Console.ReadLine());
+          
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            double quizzes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for midterm?");
+            double midterm = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for final?");
+            double final = Convert.ToDouble(Console.ReadLine());
+
+            double finalGrade = ((hw + participation + quizzes + midterm + final)/5)/100;
+           // finalGrade.ToString("P2");
+
+            Console.WriteLine(firstName + " " + lastName + " (" + studentId + "), your final grade is " + finalGrade.ToString("P2") + ".");
+
+        }
+    }
+}
